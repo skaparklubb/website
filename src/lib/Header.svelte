10a61aaf1a4380316984d2c74d085dd09b8e476a
Hans-Petter Eitvet <hpephoto@gmail.com>
2024-09-30 20:32:07 +0200

    feat: Add header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+    
+</script>
+
+<header class="flex flex-row flew-no-wrap justify-between h-16 min-h-16 bg-slate-700 m-0 w-screen p-4">
+    <div>Logo</div>
+    <div class="flex flex-row gap-4 text-slate-300 justify-center">
+        <a href="/who">Kven</a >
+        <a href="/what">Kva</a>
+        <a href="/how">Korleis</a>
+        <a href="/why">Kvifor</a>
+    </div>
+</header>
+
+<style>
+
+</style>
