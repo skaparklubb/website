571aadfc363d7caefff52727bedd22a2cc2f6bc3
hpe <hp@eitvet.no>
2025-12-08 20:18:06 +0100

    chore: Update core components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
 	import Header from '$lib/Header.svelte';
 	import '../app.css';
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
 
 <div class="container">
 	<Header></Header>
-	<slot />
+	{@render children?.()}
 </div>
